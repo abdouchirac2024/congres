0a74f1b183fbefcd4fd0c9240dec399a8fb85675
abdouchirac2024 <152296501+abdouchirac2024@users.noreply.github.com>
2025-01-04 09:49:01 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "site-bg": "#fff",
-
         separator: "#15803D",
-        // 'separator': '#67D017',
         section: "#FDA304",
         "footer-bg": "#E6FFE5",
         "text-sm": "#FDA304",
